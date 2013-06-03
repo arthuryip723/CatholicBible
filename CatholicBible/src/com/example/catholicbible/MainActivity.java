6b57927435b39a1b13d8353c80e7ead1c8184358
Arthur Yip <arthuryip723@gmail.com>
2013-06-03 08:42:59 -0400

    0602

--- CatholicBible/src/com/example/catholicbible/MainActivity.java
+++ CatholicBible/src/com/example/catholicbible/MainActivity.java
@@ -3,13 +3,40 @@ package com.example.catholicbible;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
+	GridView gridView;
+
+	static final String[] books = new String[] { "book1", "book2", "book3",
+			"book4", "book5", "book6" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		gridView = (GridView) findViewById(R.id.gridView1);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, books);
+		
+		gridView.setAdapter(adapter);
+		
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position,
+					long id) {
+				// TODO Auto-generated method stub
+				// Switch to another view
+			}
+			
+		});
 	}
 
 	@Override
@@ -18,7 +45,7 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	// my new line
 
 }
