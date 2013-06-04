d0e895944f5c92899979815cbf43465252418276
arthuryip723 <arthuryip723@gmail.com>
2013-06-04 09:52:28 -0400

    verses added

---
+++ CatholicBible/src/com/example/catholicbible/Verses.java
@@ -0,0 +1,54 @@
+package com.example.catholicbible;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Verses extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_verses);
+
+		Button backToBooksBtn = (Button) findViewById(R.id.backToBooksBtn);
+		backToBooksBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				//Intent intent = new Intent(Verses.this, MainActivity.class);
+				Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+				//intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+				//intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+
+				//finish();
+			}
+		});
+
+		Button backToChaptersBtn = (Button) findViewById(R.id.backToChaptersBtn);
+		backToChaptersBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.verses, menu);
+		return true;
+	}
+
+}
