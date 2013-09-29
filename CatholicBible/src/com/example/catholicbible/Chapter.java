f3f0c343fe71643ef0f54657a1d9a17fc699ab94
arthuryip723 <arthuryip723@gmail.com>
2013-09-29 01:01:22 -0400

    20130928

---
+++ CatholicBible/src/com/example/catholicbible/Chapter.java
@@ -0,0 +1,35 @@
+package com.example.catholicbible;
+
+public class Chapter {
+	private int id;
+	private int bookId;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return name;
+	}
+}
---
+++ CatholicBible/src/com/example/catholicbible/Verse.java
@@ -0,0 +1,36 @@
+package com.example.catholicbible;
+
+public class Verse {
+	private int id;
+	private int chapterId;
+	private String content;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getChapterId() {
+		return chapterId;
+	}
+
+	public void setChapterId(int chapterId) {
+		this.chapterId = chapterId;
+	}
+
+	@Override
+	public String toString() {
+		return content;
+	}
+}
